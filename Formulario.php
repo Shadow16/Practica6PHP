6aef40a0cb59301f45122d731b2415dce24a2cdf
Nestor Eduardo <Shadow16@users.noreply.github.com>
2014-03-19 19:53:05 -0600

    Create Formulario.php
    Práctica 6 de programación donde se crea un formulario para mandar los datos a otro archivo por medio del metodo POST

---
+++ Formulario.php
@@ -0,0 +1,31 @@
+<html>
+	<form method="POST" action="Archivo.php">
+		<table>
+			<tr>
+				<td></td>
+				<td>Nombre</td>
+				<td>Precio</td>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td><input type="text" size="10" maxlength="20" id="nombre1" name="nombre1"></td>
+				<td><input type="text" size="10" maxlength="20" id="precio1" name="precio1"></td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td><input type="text" size="10" maxlength="20" id="nombre2" name="nombre2"></td>
+				<td><input type="text" size="10" maxlength="20" id="precio2" name="precio2"></td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td><input type="text" size="10" maxlength="20" id="nombre3" name="nombre3"></td>
+				<td><input type="text" size="10" maxlength="20" id="precio3" name="precio3"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td><input type="submit" value="Enviar"></td>
+			</tr>
+		</table>
+	</form>
+</html>
